70c7ac1061c380c1c78dd8e58068d55faabfd109
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-05-10 08:58:29 +0530

    change with index.js  file

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const env = require('./config/environment');
+const environment = require('./config/environment');
 const cookieParser = require('cookie-parser');
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
 const customMware = require('./config/flashMessageMiddleware');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views')
-const db = require(env.db_path);
+const db = require(environment.db_path);
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require(env.passport_local_path);
+const passportLocal = require(environment.passport_local_path);
 const MongoStore = require('connect-mongo');
 
 app.use(expressLayouts);
 
 app.use(session({
     name: 'employeereview',
-    secret: env.secret_key,
+    secret: environment.secret_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
     store: new MongoStore({
-        mongoUrl :env.mongoose_connect,
+        mongoUrl :environment.mongoose_connect,
         autoRemve:'interval',
         autoRemoveInterval:'1'
     }),function(error){
@@ -36,7 +36,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 //now tell the app to use  static files
-app.use(express.static(env.assets_path));
+app.use(express.static(environment.assets_path));
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(flash());
